569dc5203c6ce18df70f78c69712531e95c2d9af
Dyatiend <readysteel@ya.ru>
2023-12-16 17:43:00 +0300

    Добавил тестовый эндпоинт

--- src/main/kotlin/ru/brewery/plugins/Routing.kt
+++ src/main/kotlin/ru/brewery/plugins/Routing.kt
@@ -3,11 +3,21 @@ package ru.brewery.plugins
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import ru.brewery.data.BeerModel
+import ru.brewery.data.toDto
+import ru.brewery.service.BeerService
 
 fun Application.configureRouting() {
+
+    val service = BeerService()
+
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+        get("/test") {
+            val beers = service.findAll().map(BeerModel::toDto)
+            call.respond(beers)
         }
     }
 }
