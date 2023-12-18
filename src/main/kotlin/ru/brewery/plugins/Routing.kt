dedaf2fbe2ea8ea53de45b7defcf8bd13a99c526
Dyatiend <readysteel@ya.ru>
2023-12-18 11:05:21 +0300

    Реализовал endpoint'ы

--- src/main/kotlin/ru/brewery/plugins/Routing.kt
+++ src/main/kotlin/ru/brewery/plugins/Routing.kt
@@ -1,9 +1,15 @@
 package ru.brewery.plugins
 
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
+import kotlinx.serialization.json.Json
 import ru.brewery.data.BeerModel
+import ru.brewery.data.StyleModel
+import ru.brewery.data.dto.ErrorResponse
+import ru.brewery.data.dto.FilterDtoModel
+import ru.brewery.data.dto.toFilter
 import ru.brewery.data.toDto
 import ru.brewery.service.BeerService
 
@@ -12,12 +18,67 @@ fun Application.configureRouting() {
     val service = BeerService()
 
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        get("/beers") {
+            try {
+                val filter = call.parameters["filter"]?.let { filterString ->
+                    Json.decodeFromString<FilterDtoModel>(filterString)
+                }?.toFilter()
+
+                val pageKey = call.parameters["pageKey"]?.toIntOrNull()
+                val pageSize = call.parameters["pageSize"]?.toIntOrNull()
+
+                val beers = service.findBeers(
+                    filter = filter,
+                    pageKey = pageKey,
+                    pageSize = pageSize,
+                ).map(BeerModel::toDto)
+
+                call.respond(
+                    HttpStatusCode.OK,
+                    beers,
+                )
+            } catch (_: Exception) {
+                call.respond(
+                    HttpStatusCode.BadRequest,
+                    ErrorResponse.BAD_REQUEST_RESPONSE,
+                )
+            }
         }
-        get("/test") {
-            val beers = service.findAll().map(BeerModel::toDto)
-            call.respond(beers)
+
+        get("/styles") {
+            try {
+                val pageKey = call.parameters["pageKey"]?.toIntOrNull()
+                val pageSize = call.parameters["pageSize"]?.toIntOrNull()
+
+                val styles = service.findStyles(
+                    pageKey = pageKey,
+                    pageSize = pageSize,
+                ).map(StyleModel::toDto)
+
+                call.respond(
+                    HttpStatusCode.OK,
+                    styles,
+                )
+            } catch (_: Exception) {
+                call.respond(
+                    HttpStatusCode.BadRequest,
+                    ErrorResponse.BAD_REQUEST_RESPONSE,
+                )
+            }
+        }
+
+        get("/config") {
+            try {
+                call.respond(
+                    HttpStatusCode.OK,
+                    service.createConfig().toDto(),
+                )
+            } catch (_: Exception) {
+                call.respond(
+                    HttpStatusCode.BadRequest,
+                    ErrorResponse.BAD_REQUEST_RESPONSE,
+                )
+            }
         }
     }
 }
--- src/main/kotlin/ru/brewery/service/BeerService.kt
+++ src/main/kotlin/ru/brewery/service/BeerService.kt
@@ -1,8 +1,9 @@
 package ru.brewery.service
 
-import org.litote.kmongo.KMongo
-import org.litote.kmongo.getCollection
-import ru.brewery.data.BeerModel
+import com.mongodb.client.model.Sorts
+import org.bson.conversions.Bson
+import org.litote.kmongo.*
+import ru.brewery.data.*
 
 class BeerService {
 
@@ -10,9 +11,107 @@ class BeerService {
     private val database = client.getDatabase(DATABASE_NAME)
     private val beerCollection = database.getCollection<BeerModel>(COLLECTION_NAME)
 
-    // Для тестов
-    fun findAll(): List<BeerModel> =
-        beerCollection.find().toList()
+    fun findBeers(filter: FilterModel?, pageKey: Int?, pageSize: Int?): List<BeerModel> {
+        // Список всех фильтров, которые будут применены к запросу
+        val filters = mutableListOf<Bson>()
+
+        // Применяем поисковой запрос
+        filter?.query?.let { query ->
+            // Ищем совпадения в названии пива, а также в названии стиля
+            filters.add(
+                or(
+                    (BeerModel::name).regex(query, "i"),
+                    (BeerModel::style / StyleModel::name).regex(query, "i"),
+                )
+            )
+        }
+
+        // Применяем фильтр по стилям
+        filter?.styles?.let { styles ->
+            filters.add(BeerModel::style / StyleModel::id `in` styles)
+        }
+
+        // Применяем минимальную границу крепкости
+        filter?.abvMin?.let { abvMin ->
+            filters.add(BeerModel::abv gte abvMin)
+        }
+
+        // Применяем максимальную границу крепкости
+        filter?.abvMax?.let { abvMax ->
+            filters.add(BeerModel::abv lte abvMax)
+        }
+
+        // Применяем минимальную границу горечи
+        filter?.ibuMin?.let { ibuMin ->
+            filters.add(BeerModel::ibu gte ibuMin)
+        }
+
+        // Применяем максимальную границу горечи
+        filter?.ibuMax?.let { ibuMax ->
+            filters.add(BeerModel::ibu lte ibuMax)
+        }
+
+        // Применяем флаг isOrganic
+        filter?.isOrganic?.let { isOrganic ->
+            filters.add(BeerModel::isOrganic eq if (isOrganic) "Y" else "N")
+        }
+
+        // Применяем флаг isRetired
+        filter?.isRetired?.let { isRetired ->
+            filters.add(BeerModel::isRetired eq if (isRetired) "Y" else "N")
+        }
+
+        // Выбираем поле сортировки
+        val sortField = when (filter?.sort) {
+            Sort.NAME, null -> BeerModel::name.name
+            Sort.ABV -> BeerModel::abv.name
+            Sort.IBU -> BeerModel::ibu.name
+        }
+
+        // Выбираем вариант сортировки
+        val sort = when (filter?.order) {
+            Order.ASC, null -> Sorts.ascending(sortField)
+            Order.DESC -> Sorts.descending(sortField)
+        }
+
+        // Выполняем запрос
+        return beerCollection
+            .find(and(filters))
+            .sort(sort)
+            .apply {
+                // Если переданы параметры пагинации
+                if (pageKey != null && pageSize != null) {
+                    // Передаем часть данных
+                    skip(pageKey)
+                    limit(pageSize)
+                }
+            }
+            .toList()
+    }
+
+    fun findStyles(pageKey: Int?, pageSize: Int?): List<StyleModel> {
+        return beerCollection
+            .distinct(BeerModel::style)
+            .apply {
+                // Если переданы параметры пагинации
+                if (pageKey != null && pageSize != null) {
+                    // Передаем часть данных
+                    skip(pageKey)
+                    limit(pageSize)
+                }
+            }
+            .toList()
+    }
+
+    fun createConfig(): ConfigModel {
+        val styles = beerCollection.distinct(BeerModel::style)
+        return ConfigModel(
+            abvMin = styles.minOf { style -> style.abvMin },
+            abvMax = styles.maxOf { style -> style.abvMax },
+            ibuMin = styles.minOf { style -> style.ibuMin },
+            ibuMax = styles.maxOf { style -> style.ibuMax },
+        )
+    }
 
     companion object {
 
